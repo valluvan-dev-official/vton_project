415133807f6f641bd2a390749410d6f6ebd8737d
valluvan-dev-official <btreerepo.pythonfsd@gmail.com>
2026-07-30 07:51:11 +0530

    fix(gpu_inference): use agnostic_pil instead of person_pil for image param

--- ml/scripts/gpu_inference.py
+++ ml/scripts/gpu_inference.py
@@ -257,7 +257,7 @@ class GPUInferenceEngine:
                 text_embeds_cloth=prompt_embeds_cloth.to(self.device, torch.float16),
                 cloth=garment_tensor,
                 mask_image=mask_pil,
-                image=person_pil,
+                image=agnostic_pil,
                 height=SIZE_H,
                 width=SIZE_W,
                 ip_adapter_image=garment_pil,
